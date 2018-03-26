50a05087b1aa52ee147c4146799d288dee773b40
zhoodar <joodar1993>
2018-03-26 19:03:25 +0600

    init commit

---
+++ src/main/kotlin/kg/jedi/fourangle/FourAngleApp.kt
@@ -0,0 +1,11 @@
+package kg.jedi.fourangle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FourAngleApp
+
+fun main(args: Array<String>) {
+    runApplication<FourAngleApp>(*args)
+}
